70c13b7246044469615cc6042bd17b86f6289391
tapanrawat0611 <49189987+tapanrawat0611@users.noreply.github.com>
2024-10-17 17:20:31 +0530

    Update CurrencyExchangeControllerTest.java

--- src/test/java/com/example/currency/controller/CurrencyExchangeControllerTest.java
+++ src/test/java/com/example/currency/controller/CurrencyExchangeControllerTest.java
@@ -25,7 +25,7 @@ import com.example.currency.service.DiscountService;
 class CurrencyExchangeControllerTest {
 
     @Mock
-	private CurrencyExchangeService currencyExchangeService;
+    private CurrencyExchangeService currencyExchangeService;
 	
     @Mock
     private DiscountService discountService;
